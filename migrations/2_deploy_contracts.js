58aeb3f18af838d375473757751b61108080b93d
unknown <dholms>
2017-10-20 10:55:54 +0400

    added constants file for ropsten deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Oracle = artifacts.require("./contracts/Oracle.sol");
+var Registry = artifacts.require("./contracts/Registry.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Oracle);
+  deployer.deploy(Registry);
 };
--- truffle.js
+++ truffle.js
@@ -1,9 +1,17 @@
+var constants = require("./constants");
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(constants.mnemonic, "https://ropsten.infura.io/" + constants.infura_apikey),
+      network_id: 3
     }
   }
 };
